8c2680c792ec6e738357b20130925a9e2202f11e
bitts <marcelo.valvassori@gmail.com>
2021-02-03 20:40:26 -0300

    Classe de ações do PagTesouro

--- pagtesouro.php
+++ pagtesouro.php
@@ -17,13 +17,7 @@ if(file_exists(INI_FILE)){
 }else echo "Não foi possivel abrir arquivo de configuração do Sistema";
 
 /**
- * PagTesouro class for Joomla! module
- * 
- * @package    Joomla.PagTesouro
- * @subpackage Modules
- * @link http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
- * @license        GNU/GPL, see LICENSE.php
- * mod_pagtesouro is free software. This version may have been modified pursuant
+ * This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
