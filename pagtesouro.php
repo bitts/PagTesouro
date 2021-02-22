28cb21852da64cc6e138f6c577fd816687784193
Bitts <marcelo.valvassori@gmail.com>
2021-02-22 15:20:23 -0300

    Update pagtesouro.php
    Correções finais e testes homologados em http://intranet.cms.eb.mil.br/portal/index.php/contato/pagtesouro

--- pagtesouro.php
+++ pagtesouro.php
@@ -16,7 +16,7 @@
 DEFINE("DEBUG", false );
 
 if(DEBUG){
-    ini_set('display_errors', 1); 
+    ini_set('display_errors', 1);
     ini_set('display_startup_erros', 1);
     error_reporting( E_ALL );
 }
@@ -31,22 +31,22 @@ class PagTesouro{
     public function gerar($params, $file_prm = 'pagtesouro.json'){
         $result = null;
         $dbg = new stdClass();
-        
+
         try {
-            
+
             //aqui é incrementado os dados vindos do formulário e definido o Token de acordo com o Serviço escolhido
             if (isset($params)) {
-                $params['modoNavegacao'] = "2";     
+                $params['modoNavegacao'] = "2";
                 $params['urlNotificacao'] = 'http://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
                 $params['urlRequest'] = self::$urlRequest;
                 $fields = json_encode($params);
-            
+
                 $dbg->campos = json_decode($fields);
 
                 $parametros = null;
                 if(file_exists($file_prm)){
                     $parametros = json_decode( file_get_contents($file_prm), true);
-                }else throw new Exception("Arquivo {$file_prm} não encontrado.");
+                }else $dbg->error[] = "Arquivo {$file_prm} não encontrado.";
 
                 if(!empty($parametros) && is_array($parametros)){
                     $enviar = new stdClass();
@@ -57,40 +57,37 @@ class PagTesouro{
                                 $enviar->token = $prm['token'];
                             }
                         }
-                    }
-                    $dbg->Authorization = $enviar->token;
-                }else throw new Exception("Arquivo {$file_prm} em branco ou incompleto, talvez até mesmo mal formatado. Verifique arquivo.");
-                
-            }else throw new Exception("Problemas no recebimento dos dados do formulário.");
+                    }$dbg->Authorization = $enviar->token;
+                }else $dbg->error[] = "Arquivo {$file_prm} em branco ou incompleto, talvez até mesmo mal formatado. Verifique arquivo.";
+            }else $dbg->error[] = "Problemas no recebimento dos dados do formulário.";
         } catch (Exception $e) {
             $dbg->error[] = $e->getMessage();
         }
-        
+
         if(function_exists('file_get_contents')){
             //Utilizando file_get_contents como metodo de requisição ao Pagtesouro
             try {
                 $arrContextOptions = array(
                     "ssl" => array(
                         "verify_peer" => false,
-                        "verify_peer_name" => false,
+                        "verify_peer_name" => false
                     ),
                     "http" => array(
+                        "ignore_errors" => true,
                         "method" => "POST",
                         "header" => array(
                             'Content-Type: application/json; charset=utf-8',
                             'Content-Length: ' . strlen($fields),
                             'Authorization: '. $dbg->Authorization,
-                            'Connection: close',
+                            'Connection: close'
                         ),
                         "Authorization" => $dbg->Authorization,
                         "content" => $fields
                     )
-                );
-                $context = stream_context_create($arrContextOptions);
+                );$context = stream_context_create($arrContextOptions);
                 $result = file_get_contents(self::$urlRequest, false, $context);
 
-                if(!$result)throw new Exception("Falha de conexão com o Servidor do PagTesouro");
-                else{
+                if($result){
                     $status_line = $http_response_header[0];
                     preg_match('{HTTP\/\S*\s(\d{3})}', $status_line, $match);
 
@@ -117,50 +114,44 @@ class PagTesouro{
                         'Content-Length: ' . strlen($fields),
                         'Authorization: '. $dbg->Authorization,
                         'Connection: close',
-                    ),
-                    "Authorization" => $dbg->Authorization,
+                    ),"Authorization" => $dbg->Authorization,
                     "content" => $fields
                 )
             );
-            
+
             $context = stream_context_create($arrContextOptions);
             $fp = fopen(self::$urlRequest, 'rb', false, $context);
-            if (!$fp)throw new Exception("Falha de conexão com o Servidor do PagTesouro");            
-            
-            $response = stream_get_contents($fp);
-            if ($response === false){
-                throw new Exception("Falha de conexão com o Servidor do PagTesouro");
-            }else{
-                $stream = stream_get_meta_data($fp);
-                preg_match('{HTTP\/\S*\s(\d{3})}', $stream['wrapper_data'][0], $match);
-                $dbg->result = $response;
-                $dbg->statuscode = $match[1];
-                $dbg->methodo = 'fopen';
-            }
+            if ($fp){
+                $response = stream_get_contents($fp);
+                if ($response !== false){
+                        $stream = stream_get_meta_data($fp);
+                        preg_match('{HTTP\/\S*\s(\d{3})}', $stream['wrapper_data'][0], $match);
+                        $dbg->result = $response;
+                        $dbg->statuscode = $match[1];
+                        $dbg->methodo = 'fopen';
+                }
+            } 
         }
-        
+
         if((!isset($dbg->result) || empty($dbg->result)) && function_exists('curl_init')){
             //Utilizando alternativa do curl como metodo de requisição ao Pagtesouro
             try{
 
                 $arrContextOptions = array(
-                    "Content-Type: application/json; charset=utf-8", 
+                    "Content-Type: application/json; charset=utf-8",
                     "Authorization: ". $dbg->Authorization
                 );
 
-                $ch = curl_init();                
+                $ch = curl_init();             
                 
-                curl_setopt($ch, CURLOPT_URL, self::$urlRequest);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $fields); 
-                curl_setopt($ch, CURLOPT_POST, true);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $arrContextOptions);
                 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
                 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-                        
+
                 $result = curl_exec($ch);
-                if(!$result)throw new Exception("Falha de conexão com o Servidor do PagTesouro");
+                if(!$result)$dbg->error[] = "Falha de conexão com o Servidor do PagTesouro";
                 else{
                     $dbg->result = $result;
                     $dbg->statuscode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -174,9 +165,12 @@ class PagTesouro{
             }
         }
 
+        if( !isset($dbg->result) || empty($dbg->result)) $dbg->error[] = "Não foi possível estabelecer conexão com nenhum dos metodos conhecidos.";
+
         echo json_encode($dbg);
     }
 }
+
 
 //classe de importação do layout do formulário e substituição de tag pelos serviços informador no arquivo INI
 class Template{
