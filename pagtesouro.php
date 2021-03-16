27634867962f173f74102c65c74c716ec50ca82f
Bitts <marcelo.valvassori@gmail.com>
2021-03-16 18:11:45 -0300

    Update pagtesouro.php

--- pagtesouro.php
+++ pagtesouro.php
@@ -1,6 +1,21 @@
-<?php
-defined('_JEXEC') or die;
 
+<?php
+
+/*
+# Title: Sisteminha mixuruca de integração ao PagTesouro
+# Description: sistema para geração de GRU do PagTesouro
+# Author: Marcelo Valvassori Bittencourt
+# E-mail: marcelo.valvassori.bittencourt@gmail.com
+# version: 2.0
+# Modificação: 16 MAR 2021
+*
+* This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+*/
+
+defined('_JEXEC') or die;
 
 try {
 
